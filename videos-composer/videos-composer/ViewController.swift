b036aa5ff90e00b84267002c08991c86306b4b5e
Romain Roche <romain.roche@news-republic.com>
2017-09-04 15:21:36 +0200

    cosmetics

--- videos-composer/videos-composer/ViewController.swift
+++ videos-composer/videos-composer/ViewController.swift
@@ -7,30 +7,69 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBOutlet private var firstButton: UIButton?;
-    @IBOutlet private var secondButtin: UIButton?;
+    @IBOutlet private var firstButton: UIButton?
+    @IBOutlet private var secondButtin: UIButton?
+    @IBOutlet private var capturedVideoView: UIView?
+    
+    private var capturedVideo: NSData?
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let moviePath = info[UIImagePickerControllerMediaURL] as! URL!
+        self.installCapturedVideo(moviePath)
+        self.dismiss(animated: true, completion: {})
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: {})
+    }
 
     @IBAction
     private func captureFirstVideo() {
-        print("capturing first video");
+        
+        if (!UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)) {
+            print("no camera available")
+            return
+        }
+        
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.camera
+        picker.mediaTypes = [kUTTypeMovie as String]
+        self.present(picker, animated: true)
+        
     }
     
     @IBAction
     private func getSecondVideo() {
-        print("getting second video");
+        print("getting second video")
+    }
+    
+    private func installCapturedVideo(_ imageURL: URL!) {
+        self.player?.pause()
+        self.playerLayer?.removeFromSuperlayer()
+        self.player = AVPlayer(url: imageURL)
+        self.playerLayer = AVPlayerLayer(player: player)
+        self.playerLayer!.frame = (self.capturedVideoView?.bounds)!
+        self.capturedVideoView?.layer.addSublayer(self.playerLayer!)
+        self.player?.isMuted = true // no sound
+        self.player?.play()
     }
 
 }
