d873980f612c0cb84f3a933e068308b0e040d153
Romain Roche <romain.roche@news-republic.com>
2017-09-05 10:08:29 +0200

    cosmetics

--- videos-composer/videos-composer/ViewController.swift
+++ videos-composer/videos-composer/ViewController.swift
@@ -57,7 +57,6 @@ class VCCaptureViewController: UIViewController, UIImagePickerControllerDelegate
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: UIImagePickerControllerDelegate
